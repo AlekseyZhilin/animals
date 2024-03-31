9ee2939a1406b6d0f6b8306c97017af44701324b
Aleksey <alekseyzhilin@mail.ru>
2024-03-31 17:02:23 +0300

    завершение классов

--- animals_class.py
+++ animals_class.py
@@ -12,10 +12,24 @@ class InterfaceAnimals:
     def add_command(self, command: str):
         self.commands_list.append(command)
 
+    def add_commands(self, commands_lisr: list):
+        for i in commands_lisr:
+            self.commands_list.append(i)
+
+    def get_commands_str(self) -> str:
+        commands = ' ('
+        for i in self.commands_list:
+            commands += ' ' + i
+        commands += ' )'
+        return commands
+
     def show_commands(self):
         print(f'Команды выполняемые {self.name}:')
         for i, cmd in enumerate(self.commands_list):
             print(f'{i}) {cmd}')
+
+    def __gt__(self, other):
+        return self.birth_date > other.birth_date
 
 class Dog(InterfaceAnimals):
     count = 0
@@ -23,10 +37,10 @@ class Dog(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[0]
         self.vid = vid_animals[0]
-        Dog.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 class Cat(InterfaceAnimals):
     count = 0
@@ -34,10 +48,10 @@ class Cat(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[1]
         self.vid = vid_animals[0]
-        Cat.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 
 class Hamster(InterfaceAnimals):
@@ -47,10 +61,10 @@ class Hamster(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[2]
         self.vid = vid_animals[0]
-        Hamster.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 class Horse(InterfaceAnimals):
     count = 0
@@ -59,10 +73,10 @@ class Horse(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[3]
         self.vid = vid_animals[1]
-        Horse.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 class Camel(InterfaceAnimals):
     count = 0
@@ -71,10 +85,10 @@ class Camel(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[4]
         self.vid = vid_animals[1]
-        Camel.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 class Donkey(InterfaceAnimals):
     count = 0
@@ -83,10 +97,10 @@ class Donkey(InterfaceAnimals):
         super().__init__(name, birth_date)
         self.type = type_animals[5]
         self.vid = vid_animals[1]
-        Donkey.count += 1
+        type(self).count += 1
 
     def __str__(self):
-        return f'{self.name}, {self.birth_date}, {self.type}, {self.vid}'
+        return f'{self.name: <10s} {self.birth_date}, {self.type}, {self.vid} {self.get_commands_str()}'
 
 def make_dog(name, birth_date):
     return Dog(name, birth_date)
@@ -111,7 +125,7 @@ class AnimalsList:
         self.animals_list = []
         
     def add_animal(self, animal: InterfaceAnimals) -> bool:
-        if animal.name not in self.animals_list:
+        if animal.name not in [i.name for i in self.animals_list]:
             self.animals_list.append(animal)
             return True
         
@@ -122,11 +136,29 @@ class AnimalsList:
         for i in list_animals:
             self.add_animal(i)
 
+    def find_animal(self, name: str) -> InterfaceAnimals:
+        for i in self.animals_list:
+            if name == i.name:
+                return i
+        return None
+
     def show_animals(self):
         print('Список всех животных:')
         for i, animal in enumerate(self.animals_list):
             print(f'{i + 1}) {animal}')
 
+    def show_count_animals(self):
+        list_typs = []
+        print('Количество животных:')
+        for i, type_animal, count in [(i, animal.type, type(animal).count)
+                                      for i, animal in enumerate(self.animals_list)]:
+            if type_animal not in list_typs:
+                list_typs.append(type_animal)
+                print(f'{i + 1}) {type_animal: <10s} {count}')
 
+    def sort_by_birth_date(self):
+        print('Список всех животных, отсортированных по возрасту:')
+        for i, animal in enumerate(sorted(self.animals_list)):
+            print(f'{i + 1}) {animal}')
 
     
--- main_class.py
+++ main_class.py
@@ -1,13 +1,29 @@
 from animals_class import make_dog, make_cat, make_hamster, make_horse, make_camel, make_donkey
 from animals_class import AnimalsList
 
+commands_array = ('сидеть', 'лежать', 'голос', 'вперед', 'назад')
+
 def main():
-    dog1 = make_dog('шарик', '2020-01-01')
-    dog2 = make_dog('рэй', '2022-01-01')
+    dog1    = make_dog('шарик', '2021-07-01')
+    dog1.add_commands([commands_array[0], commands_array[1], commands_array[2]])
+    dog2    = make_dog('рэй', '2018-01-01')
+    dog2.add_command(commands_array[2])
+    cat     = make_cat('мурка', '2020-01-01')
+    hamster = make_hamster('хомя','2022-01-01')
+    horse   = make_horse('быстрый', '2022-02-01')
+    camel   = make_camel('странник', '2019-01-01')
+    donkey  = make_donkey('бяша', '2020-01-01')
 
     animals_list = AnimalsList()
-    animals_list.add_animal(dog1)
-    animals_list.add_animal(dog2)
+    animals_list.add_animals([dog1, dog2])
+    animals_list.add_animal(cat)
+    animals_list.add_animal(hamster)
+    animals_list.add_animals([horse, camel, donkey])
+    anm = animals_list.find_animal('бяша')
+    anm.add_commands([commands_array[3], commands_array[4]])
+
     animals_list.show_animals()
+    animals_list.show_count_animals()
+    animals_list.sort_by_birth_date()
 
 main()
